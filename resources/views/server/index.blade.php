a3a9f75993657e39e0218ef40896532c56f84f2c
vicky <pratamavr@gmail.com>
2025-12-05 00:46:43 +0800

    add networks

--- resources/views/server/index.blade.php
+++ resources/views/server/index.blade.php
@@ -489,7 +489,7 @@
           this.networksHistory.shift();
         }
         
-        this.charts.networks.data.labels = Object.keys(this.networksHistory.time);
+        this.charts.networks.data.labels = this.networksHistory.time;
         
         const networkData = [{
           data: this.networksHistory.received,
