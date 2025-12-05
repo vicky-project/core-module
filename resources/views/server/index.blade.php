cfc70acdd348106a1c6d30823c52e69abc8c2d3c
vicky <pratamavr@gmail.com>
2025-12-05 08:42:47 +0800

    add networks

--- resources/views/server/index.blade.php
+++ resources/views/server/index.blade.php
@@ -150,8 +150,8 @@
               <div class="float-start me-auto">
                 Interface: <span id="network-interface" class="font-weight-bold"></span>
               </div>
-              <p>Sent: <span id="network-sent" class="text-bg-warning">0Kbps</span></p>
-              <p>Received: <span id="network-received" class="text-bg-primary">0kbps</span></p>
+              <p>Sent: <span id="network-sent" class="text-warning">0Kbps</span></p>
+              <p>Received: <span id="network-received" class="text-primary">0kbps</span></p>
             </div>
           </div>
           <div class="row mb-2">
