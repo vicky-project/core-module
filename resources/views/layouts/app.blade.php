5b2e2d5a54042b479ca981d278b1b2514698d6d2
vicky <pratamavr@gmail.com>
2025-12-19 00:23:11 +0800

    refactor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,6 @@
           </div>
         </main>
         <x-core-footer />
-        @include('viewmanager::partials.footer')
       </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
