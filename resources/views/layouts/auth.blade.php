b2e0c3b67817701b80c900fbf83d66583274c376
vicky <pratamavr@gmail.com>
2026-02-02 23:06:21 +0800

    updates

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -44,5 +44,7 @@
       </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    
+    @stack('scripts')
   </body>
 </html>
