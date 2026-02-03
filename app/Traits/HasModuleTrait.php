06bb0b2757c2402dcda69f1d27ad83498d9c4b1c
vicky <pratamavr@gmail.com>
2026-02-03 20:43:42 +0800

    updates

--- app/Traits/HasModuleTrait.php
+++ app/Traits/HasModuleTrait.php
@@ -6,14 +6,53 @@ use Nwidart\Modules\Facades\Module;
 
 trait HasModuleTrait
 {
-	public static function boot()
+	public static function bootHasModuleTrait()
 	{
-		parent::boot();
+		static::mergeFillable(static::getModuleFillableAtributes());
+		static::mergeCasts(static::getModuleCasts());
 
-		static::boot(function ($model) {
-			$model
-				->mergeFillable(["telegram_id", "telegram_username", "auth_date"])
-				->mergeCasts(["auth_date" => "timestamp"]);
+		static::setupModuleExtensions();
+	}
+
+	protected static function getModuleFillableAtributes()
+	{
+		$fillable = ["telegram_id", "telegram_username", "auth_date"];
+
+		$modules = Module::allEnabled();
+
+		foreach ($modules as $module) {
+			$config = Module::config(
+				$module->getLowerName() . ".table_fields.fillable",
+				[]
+			);
+
+			$fillable = array_merge($fillable, $config);
+		}
+
+		return array_unique($fillable);
+	}
+
+	protected static function getModuleCasts()
+	{
+		$casts = ["auth_date" => "timestamp"];
+
+		$modules = Module::allEnabled();
+
+		foreach ($modules as $module) {
+			$config = Module::config(
+				$module->getLowerName() . "table_fields.casts",
+				[]
+			);
+			$casts = array_merge($casts, $config);
+		}
+
+		return $casts;
+	}
+
+	protected static function setupModuleExtensions()
+	{
+		static::booted(function ($model) {
+			event(new \Modules\Core\Events\UserModelBooted());
 		});
 	}
 }
