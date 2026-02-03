7fd3ed73bf51e6c2ec24d788741ee6d729f7c7cc
vicky <pratamavr@gmail.com>
2026-02-03 16:37:02 +0800

    updates

--- app/Traits/HasModuleTrait.php
+++ app/Traits/HasModuleTrait.php
@@ -2,27 +2,16 @@
 
 namespace Modules\Core\Traits;
 
-use Modules\Core\Events\ModuleTraitEvent;
 use Nwidart\Modules\Facades\Module;
 
 trait HasModuleTrait
 {
-	public static function bootHasModuleTrait()
+	protected $mergeFillable = ["telegram_id", "telegram_username", "auth_date"];
+
+	public function boot()
 	{
-		static::booted(function ($model) {
-			$modules = Module::allEnabled();
-
-			foreach ($modules as $module) {
-				$config = Module::config($module->getLowerName());
-
-				if (isset($config["model_extensions"])) {
-					foreach ($config["model_extensions"] as $extensionClass) {
-						if (class_exists($extensionClass)) {
-							$extensionClass::extend($model);
-						}
-					}
-				}
-			}
+		static::boot(function ($model) {
+			$model->mergeFillable($this->mergeFillable);
 		});
 	}
 }
