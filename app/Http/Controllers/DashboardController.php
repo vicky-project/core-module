02c4c849fceba40a09a79b8c2b5f3aaaf3844cfd
vicky <pratamavr@gmail.com>
2026-02-03 12:21:23 +0800

    updates

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -78,6 +78,7 @@ class DashboardController extends BaseController
 		$data_check_string = implode("\n", $data_check_arr);
 		$secret_key = hash("sha256", $bot_token, true);
 		$hash = hash_hmac("sha256", $data_check_string, $secret_key);
+		dd($data_check_string, $data_check_arr, $hash);
 		if (strcmp($hash, $check_hash) !== 0) {
 			throw new \Exception("Data is NOT from Telegram");
 		}
