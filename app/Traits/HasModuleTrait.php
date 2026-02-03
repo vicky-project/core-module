3adc32fff7d781444518ca2b23d9878ab1c3cf61
vicky <pratamavr@gmail.com>
2026-02-03 21:10:12 +0800

    updates

--- app/Traits/HasModuleTrait.php
+++ app/Traits/HasModuleTrait.php
@@ -8,25 +8,33 @@ trait HasModuleTrait
 {
 	public static function bootHasModuleTrait()
 	{
-		static::mergeFillable(static::getModuleFillableAtributes());
-		static::mergeCasts(static::getModuleCasts());
+		static::booted(function ($model) {
+			$model->mergeFillable(static::getModuleFillableAtributes());
+			$model->mergeCasts(static::getModuleCasts());
 
-		static::setupModuleExtensions();
+			static::setupModuleExtensions($model);
+		});
 	}
 
 	protected static function getModuleFillableAtributes()
 	{
 		$fillable = ["telegram_id", "telegram_username", "auth_date"];
 
-		$modules = Module::allEnabled();
+		try {
+			$modules = Module::allEnabled();
 
-		foreach ($modules as $module) {
-			$config = Module::config(
-				$module->getLowerName() . ".table_fields.fillable",
-				[]
+			foreach ($modules as $module) {
+				$config = Module::config(
+					$module->getLowerName() . ".table_fields.fillable",
+					[]
+				);
+
+				$fillable = array_merge($fillable, $config);
+			}
+		} catch (\Exception $e) {
+			throw new \Exception(
+				"Failed to get Module fillabel attributes: " . $e->getMessage()
 			);
-
-			$fillable = array_merge($fillable, $config);
 		}
 
 		return array_unique($fillable);
@@ -36,14 +44,20 @@ trait HasModuleTrait
 	{
 		$casts = ["auth_date" => "timestamp"];
 
-		$modules = Module::allEnabled();
+		try {
+			$modules = Module::allEnabled();
 
-		foreach ($modules as $module) {
-			$config = Module::config(
-				$module->getLowerName() . "table_fields.casts",
-				[]
+			foreach ($modules as $module) {
+				$config = Module::config(
+					$module->getLowerName() . "table_fields.casts",
+					[]
+				);
+				$casts = array_merge($casts, $config);
+			}
+		} catch (\Exception $e) {
+			throw new \Exception(
+				"Failed to get Module casts attribute: " . $e->getMessage()
 			);
-			$casts = array_merge($casts, $config);
 		}
 
 		return $casts;
@@ -51,8 +65,6 @@ trait HasModuleTrait
 
 	protected static function setupModuleExtensions()
 	{
-		static::booted(function ($model) {
-			event(new \Modules\Core\Events\UserModelBooted());
-		});
+		event(new \Modules\Core\Events\UserModelBooted());
 	}
 }
