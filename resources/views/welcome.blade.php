15af4a981c7ffb162a553e28b442d0cec0416268
vicky <pratamavr@gmail.com>
2026-02-16 14:48:27 +0800

    updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,35 +1,38 @@
 @extends('core::layouts.main')
 
 @section('content')
-<!-- Logo Lingkaran -->
-<div class="app-logo">
-  <img src="{{ config('core.logo_url') }}" alt="Logo Aplikasi" class="img-fluid rounded-circle">
-</div>
+<div class="main-container">
+  
+  <!-- Logo Lingkaran -->
+  <div class="app-logo mt-4 align-items-center justify-content-center">
+    <img src="{{ config('core.logo_url') }}" alt="Logo Aplikasi" class="img-fluid rounded-circle">
+  </div>
 
-<!-- Nama Aplikasi -->
-<div class="app-name h4 fw-bold text-center">
-  {{ config('app.name') }} App
-</div>
+  <!-- Nama Aplikasi -->
+  <div class="app-name h4 fw-bold text-center">
+    {{ config('app.name') }} App
+  </div>
 
-<!-- Deskripsi -->
-<div class="app-description text-center">
-  <small>
-    Satu aplikasi untuk semua fitur tersedia.
-  </small>
-</div>
+  <!-- Deskripsi -->
+  <div class="app-description text-center">
+    <small>
+      Satu aplikasi untuk semua fitur tersedia.
+    </small>
+  </div>
         
-<!-- Menu Utama -->
-@hasHook('main-apps')
-<div class="container text-center mt-4 p-3">
-  <div class="row">
-    @hook('main-apps')
-    <div class="col-4 col-md-2 mb-2">
-      <a onclick="handleMenuClick('pengaturan');" class="menu-item rounded-4 p-2">
-        <i class="bi bi-gear"></i>
-        <span>Pengaturan</span>
-      </a>
+  <!-- Menu Utama -->
+  @hasHook('main-apps')
+  <div class="container text-center mt-4 p-3">
+    <div class="row">
+      @hook('main-apps')
+      <div class="col-4 col-md-2 mb-2">
+        <a onclick="handleMenuClick('pengaturan');" class="menu-item rounded-4 p-2">
+          <i class="bi bi-gear"></i>
+          <span>Pengaturan</span>
+        </a>
+      </div>
     </div>
   </div>
+  @endHasHook
 </div>
-@endHasHook
 @endsection
