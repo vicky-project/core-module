66f1e25a0c6874a4283306c60f007debb8c3be88
vicky <pratamavr@gmail.com>
2026-02-20 00:08:05 +0800

    updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     
     @include('core::partials.app-styles')
+    @stack('styles')
 </head>
 <body>
   @yield('content')
@@ -108,4 +109,6 @@
 
       tg.ready();
     </script>
+    
+    @stack('scripts')
 </body>
