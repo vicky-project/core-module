355f208a16e6a4ac7946f521ba0c630ad1d6db00
vicky <pratamavr@gmail.com>
2026-02-19 21:49:49 +0800

    updates

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -33,9 +33,13 @@
         document.body.style.setProperty('--tg-theme-button-text-color', theme.button_text_color || '#ffffff');
         document.body.style.setProperty('--tg-theme-secondary-bg-color', theme.secondary_bg_color || '#f0f0f0');
         document.body.style.setProperty('--tg-theme-section-bg-color', theme.section_bg_color || '#f0f0f0');
+        @if(Route::has('settings.index'))
         tg.SettingsButton.isVisible = true;
+        tg.SettingsButton.onClick(() => window.location.href = '{{ route("settings.index") }}');
         tg.SettingsButton.show();
+        @endif
         tg.BackButton.isVisible = true;
+        tg.BackButton.onClick(() => window.location.href = '{{ back() }}');
         tg.BackButton.show();
         
         //tg.setHeaderColor()
