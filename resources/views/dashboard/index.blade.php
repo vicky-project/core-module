6498169623008a3ff441f60115af8e2dab68dad8
vicky <pratamavr@gmail.com>
2026-02-20 00:05:46 +0800

    updates

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -34,12 +34,11 @@
 
 <div class="content container-custom">
   <!-- Grid Aplikasi -->
-  <div class="section-title">Aplikasi</div>
   <div class="app-grid">
     @hasHook('dashboard-widgets')
       @hook('dashboard-widgets')
     @endHasHook
-    <a href="#" class="app-item">
+    <a href="#" class="app-item" onclick="navigateTo('akun')">
       <i class="bi bi-person-circle"></i>
       <span>Akun</span>
     </a>
